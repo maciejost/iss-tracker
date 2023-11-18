73436b78cab6214254dca1fc6003edc4d8df5b14
Ole Jørgen <ole.jorgen.bakken@fremtind.no>
2023-11-18 20:12:45 +0100

    😎

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className='text-4xl'>Caroline & Maciej mekker apps</h1>
+      <h1 className='text-4xl'>Caroline & Maciej & Ole Jørgen mekker apps</h1>
     </main>
   )
 }
