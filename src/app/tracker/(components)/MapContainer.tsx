49d5c5f1e2ac39e06b0107a549ea1f0d0f252a7d
Maciej Ostrowski <maciej.ostrowski@fremtind.no>
2023-11-20 13:01:07 +0100

    Fiks dynamisk import av kart.

--- src/app/tracker/(components)/MapContainer.tsx
+++ src/app/tracker/(components)/MapContainer.tsx
@@ -1,4 +1,5 @@
 "use client"
+
 import { Coordinates } from "@/app/api/iss-location/route";
 import { useEffect, useState } from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
@@ -6,25 +7,34 @@ import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 export default function OurMapContainer({ coordinates }: {
  coordinates: Coordinates;
 }) {
- const [isClient, setIsClient] = useState(false);
- useEffect(() => setIsClient(true), []);
+  const [isMounted, setIsMounted] = useState(false);
 
- if (!window || !isClient) return null;
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
 
  return (
-  <MapContainer center={[
-   coordinates.lat,
-   coordinates.lon
-  ]} zoom={13} scrollWheelZoom={false}>
-   <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-   />
-   <Marker position={[51.505, -0.09]}>
-    <Popup>
-     A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
-   </Marker>
-  </MapContainer>
+  <>
+    {
+      isMounted && (
+        <MapContainer center={[
+          coordinates.lat,
+          coordinates.lon
+          ]} zoom={13} scrollWheelZoom={false}
+          style={{ height: "100%", width: "100%" }}
+        >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[51.505, -0.09]}>
+          <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        </MapContainer>
+        )
+    }
+  </>
  )
 }
--- src/app/tracker/page.tsx
+++ src/app/tracker/page.tsx
@@ -1,12 +1,22 @@
+import { useMemo } from 'react';
 import OurMapContainer from "./(components)/MapContainer";
+import dynamic from 'next/dynamic';
+
+
 
 export default function Tracker() {
  /* const data = await fetch("http://localhost:3000/api/iss-location");
  const coordinates = await data.json() as Coordinates; */
 
+ const Map = useMemo(
+  () => dynamic(() => import("./(components)/MapContainer"), { ssr: false }),
+  []
+);
+
+
  return (
-  <div className=" h-20">
-   <OurMapContainer
+  <div className=" h-96">
+   <Map
     coordinates={{
      lat: 51.505,
      lon: -0.09
